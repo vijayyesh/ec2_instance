7bb8bb3bd016ce03d2904f78bb3f8744e473ac3b
vijayyesh <95198082+vijayyesh@users.noreply.github.com>
2022-03-05 18:28:39 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+    region =   var.aws_region
+    profile = var.profile_name
+}
+resource "aws_instance" "ec2-1" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  subnet_id = var.subnet2a
+  count = var.condition == true ? 1 : 0
+  tags = {
+    Name = "ec2-web1"
+  }
+}
+resource "aws_instance" "ec2-2" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  subnet_id = var.subnet2a
+  count = var.condition == false ? 1 : 0
+  tags = {
+    Name = "ec2-web2"
+  }
+}
+
