87ab83f4cc9ec11e7fece8d2c4646d41b614e172
vijayyesh <95198082+vijayyesh@users.noreply.github.com>
2022-03-05 19:32:44 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,22 +1,25 @@
 variable "aws_region" {
-    default = "eu-west-2"
+    default = "us-east-1"
   }
 
 variable "key_name" {
-  default = "youtube"
+  default = "mykey"
 }
-  variable "ec2_types" {
-    default = "t2.micro"
+  variable "region_ami" {
+    type =map
+      
+    default = {
+     us-east-1= "ami-0c293f3f676ec4f90"
+     us=east-2= "ami-03a0c45ebc70f98ea"
   }
 
-variable "region_ami" {
+variable "region_instance_type" {
   type = map
 
   default = {
-   eu-west-1 = "ami-06178cf087598769c",
-   eu-west-2 = "ami-08b993f76f42c3e2f",
-   eu-west-3 = "ami-0083960c1530c641f",
-   us-east-1 = "ami-0a01a5636f3c4f21c"
+   us-east-1= "t2.nano",
+   us-east-2= "t2.micro"
+   
   }
 }
 
